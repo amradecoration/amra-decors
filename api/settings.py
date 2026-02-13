c0a3177f22c01233f06bd92f650768e8e06e166b
Mohamed Siddiq <siddiq@cyberheals.com>
2026-02-13 22:59:11 +0530

    Delete: Vercel insights middleware

--- api/settings.py
+++ api/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    # 'api.middleware.VercelSpeedInsightsMiddleware',
 ]
 
 ROOT_URLCONF = 'api.urls'
